2d9e96c969aec362dabb0e97fe682cbf6457015a
egor.mikhalev <mamo4ka21196@gmail.com>
2024-11-12 19:26:41 +0300

    added gradle dependencies

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -7,8 +7,9 @@ import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidApplication)
-    alias(libs.plugins.composeMultiplatform)
-    alias(libs.plugins.composeCompiler)
+    alias(libs.plugins.jetbrainsCompose)
+    alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.apollo)
 }
 
 kotlin {
